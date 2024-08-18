f7f9508ca93af07ca63708974e75424ac75b71f0
Tama0026 <mtam7896@gmail.com>
2024-08-18 16:09:39 +0700

    Add project files.

---
+++ GameMenu/GameListWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GameMenu
+{
+    public class Game
+    {
+        public string Name { get; set; }
+        public string ImagePath { get; set; }
+    }
+
+    public partial class GameListWindow : Window
+    {
+        public GameListWindow(List<Game> games)
+        {
+            InitializeComponent();
+            gameList.ItemsSource = games; // Set the ItemsSource to the list of games passed in
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button playButton = sender as Button;
+            string selectedGame = playButton.Tag as string;
+
+            if (!string.IsNullOrEmpty(selectedGame))
+            {
+                StartGame(selectedGame);
+                this.Close(); // Close the window after selecting a game
+            }
+        }
+
+        private void StartGame(string gameName)
+        {
+            switch (gameName)
+            {
+                case "Space Shooter":
+                    StartSpaceShooter();
+                    break;
+                case "Snake":
+                    StartSnake();
+                    break;
+                case "Tetris":
+                    StartTetris();
+                    break;
+                case "FlappyBird":
+                    StartFlappyBird();
+                    break;
+                case "Memory":
+                    StartMemory();
+                    break;
+                case "Chess":
+                    StartChess();
+                    break;
+                default:
+                    MessageBox.Show("Game not found!");
+                    break;
+            }
+        }
+
+        private void StartSpaceShooter()
+        {
+            SpaceShooterGame game1 = new SpaceShooterGame();
+            game1.Show();
+        }
+
+        private void StartSnake()
+        {
+            Snake snake = new Snake();
+            snake.Show();
+        }
+
+        private void StartTetris()
+        {
+            Tetris game1 = new Tetris();
+            game1.Show();
+        }
+        private void StartFlappyBird()
+        {
+            FlappyBird game1 = new FlappyBird();
+            game1.Show();
+        }
+        private void StartChess()
+        {
+            ChessUI.MainWindow game1 = new ChessUI.MainWindow();
+            game1.Show();
+        }
+        private void StartMemory()
+        {
+            Memory.MainWindow game1 = new Memory.MainWindow();
+            game1.Show();
+        }
+    }
+}
---
+++ GameMenu/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace GameMenu
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void one_player_btn_Click(object sender, RoutedEventArgs e)
+        {
+            // Define single-player games with their images
+            var singlePlayerGames = new List<Game>
+            {
+                new Game { Name = "Space Shooter", ImagePath = "Logo_Game/space_shooter.jpg" },
+                new Game { Name = "FlappyBird", ImagePath = "Logo_Game/flappybird.jpg" },
+                new Game { Name = "Snake", ImagePath = "Logo_Game/snake.png" },
+            };
+
+            // Pass the list of games to the GameListWindow
+            GameListWindow gameListWindow = new GameListWindow(singlePlayerGames);
+            gameListWindow.Show();
+        }
+
+        private void two_player_btn_Click(object sender, RoutedEventArgs e)
+        {
+            // Define multi-player games with their images
+            var multiPlayerGames = new List<Game>
+            {
+                new Game { Name = "Chess", ImagePath = "Logo_Game/chess.jpg" },
+                new Game { Name = "Tetris", ImagePath = "Logo_Game/tetris.jpg" },
+                new Game { Name = "Memory", ImagePath = "Logo_Game/memory.png" }
+
+            };
+
+            // Pass the list of games to the GameListWindow
+            GameListWindow gameListWindow = new GameListWindow(multiPlayerGames);
+            gameListWindow.Show();
+        }
+
+        private void exit_btn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Do you want to exit?", "Confirm exit", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+    }
+}
